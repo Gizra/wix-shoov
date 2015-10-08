5000af5ad697401669243e4e4e9fe8e2d7d7189b
Yuri Failer <yuri@gizra.com>
2015-10-08 12:26:38 +0300

    WIP.

--- behat/features/bootstrap/FeatureContext.php
+++ behat/features/bootstrap/FeatureContext.php
@@ -35,4 +35,93 @@ class FeatureContext extends MinkContext implements SnippetAcceptingContext {
   public function iShouldNotHaveAccessToThePage() {
     $this->assertSession()->statusCodeEquals('403');
   }
+
+  /**
+   * @Then I select an option
+   */
+  public function iSelectAnOption() {
+    $this->iWaitForCssElement('.dynamicElements');
+
+//    $element = $this->getSession()->getPage()->find('css', '.radioOption');
+    $element = $this->getSession()->getPage()->find('css', '.radioOption:has(input[name="(Radio 3)"])');
+    $element->click();
+
+    $button = $this->getSession()->getPage()->find('css', '#submitButton');
+    $button->click();
+//
+  }
+
+  /**
+   * @Then I should see the text :arg1 message
+   */
+  public function iShouldSeeTheTextMessage($text) {
+    $this->iWaitForCssElement('.success');
+    $this->assertPageContainsText($text);
+  }
+
+
+  /**
+   * @Then I should wait for the text :arg1 to :arg2
+   */
+  public function iShouldWaitForTheTextTo($text, $appear) {
+    $this->waitForXpathNode(".//*[contains(normalize-space(string(text())), \"$text\")]", $appear == 'appear');
+  }
+
+  /**
+   * @Then /^I wait for css element "([^"]*)" to "([^"]*)"$/
+   */
+  public function iWaitForCssElement($element, $appear = 'appear') {
+    $xpath = $this->getSession()->getSelectorsHandler()->selectorToXpath('css', $element);
+    $this->waitForXpathNode($xpath, $appear == 'appear');
+  }
+
+  /**
+   * Helper function; Execute a function until it return TRUE or timeouts.
+   *
+   * @param $fn
+   *   A callable to invoke.
+   * @param int $timeout
+   *   The timeout period. Defaults to 10 seconds.
+   *
+   * @throws Exception
+   */
+  private function waitFor($fn, $timeout = 15000) {
+    $start = microtime(true);
+    $end = $start + $timeout / 1000.0;
+    while (microtime(true) < $end) {
+      if ($fn($this)) {
+        return;
+      }
+    }
+    throw new \Exception('waitFor timed out.');
+  }
+
+  /**
+   * Wait for an element by its XPath to appear or disappear.
+   *
+   * @param string $xpath
+   *   The XPath string.
+   * @param bool $appear
+   *   Determine if element should appear. Defaults to TRUE.
+   *
+   * @throws Exception
+   */
+  private function waitForXpathNode($xpath, $appear = TRUE) {
+    $this->waitFor(function($context) use ($xpath, $appear) {
+      try {
+        $nodes = $context->getSession()->getDriver()->find($xpath);
+        if (count($nodes) > 0) {
+          $visible = $nodes[0]->isVisible();
+          return $appear ? $visible : !$visible;
+        }
+        return !$appear;
+      }
+      catch (WebDriver\Exception $e) {
+        if ($e->getCode() == WebDriver\Exception::NO_SUCH_ELEMENT) {
+          return !$appear;
+        }
+        throw $e;
+      }
+    });
+  }
 }
