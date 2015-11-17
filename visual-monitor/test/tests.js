a1913c4ff2ecca17fc438f0b537b7acc9a01c018
David Kohav <david@gizra.com>
2015-11-17 13:38:16 +0200

    #1 Add visual monitor for the wix apps

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -47,37 +47,63 @@ describe('Visual monitor testing', function() {
     shoovWebdrivercss.after(done);
   });
 
-  it('should show the Currency Preview page',function(done) {
+  it('should show the sunglasses page',function(done) {
+    client
+      .url('http://www.wix.com/demone2/eyewear-store#!sunglasses/cuba')
+      .webdrivercss(testName + '.sunglasses', {
+        name: '1',
+        screenWidth: selectedCaps == 'chrome' ? [1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  it('should show the optics page',function(done) {
+    client
+      .url('http://www.wix.com/demone2/eyewear-store#!optics/c1no')
+      .webdrivercss(testName + '.optics', {
+        name: '1',
+        screenWidth: selectedCaps == 'chrome' ? [1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
+  it('should show the sale page',function(done) {
     client
       .url('http://www.wix.com/demone2/eyewear-store#!sale/c1ijr')
-      .webdrivercss(testName + '.currency-preview', {
+      .webdrivercss(testName + '.sale', {
         name: '1',
         screenWidth: selectedCaps == 'chrome' ? [1200] : undefined,
       }, resultsCallback)
       .call(done);
   });
 
-  it('should show the rio page',function(done) {
+  it('should show the join our mailing list  page',function(done) {
     client
-      .url('http://www.wix.com/demone2/rio-pousada')
-      .webdrivercss(testName + '.rio', {
+      .url('http://shoutout.apps.wixapps.net/widget-new?cacheKiller=1447749091460&compId=comp-iemmphes&deviceType=desktop&instance=T8dxpqxIILlphorhjg1X6IkiZsMpPb66sUL2F5c5Luk.eyJpbnN0YW5jZUlkIjoiMTQwY2JjMGQtYWFjZi1iYTc3LTljOGMtZjMwMjUyNjE4ZTlkIiwic2lnbkRhdGUiOiIyMDE1LTExLTE3VDA4OjMxOjMwLjUzNVoiLCJ1aWQiOm51bGwsInBlcm1pc3Npb25zIjpudWxsLCJpcEFuZFBvcnQiOiI4NC4xMDkuOTAuODgvNjUyOTMiLCJ2ZW5kb3JQcm9kdWN0SWQiOm51bGwsImRlbW9Nb2RlIjp0cnVlLCJvcmlnaW5JbnN0YW5jZUlkIjoiMTQwYzBiZDktN2NlYy05YWU5LWI5MjMtZGYyZmUyNGQxYWRmIiwiYWlkIjoiMmFlNjc5ZGItNzM0Yi00YzAxLWFjOTQtMmRkYmUwNTI4YzY2IiwiYmlUb2tlbiI6IjM3N2VlYjExLTIwYjItZmZlMS0xZDYxLWE4YTIzYzZhNjFhOSIsInNpdGVPd25lcklkIjoiODQ3NzBmNjctZWNiZC00NGI2LWIzNWEtNTg0ZjJkYzE1YWYxIn0&locale=en&viewMode=site&width=275%22')
+      .webdrivercss(testName + '.join-our-mailing-list', {
         name: '1',
-        screenWidth: selectedCaps == 'chrome' ? [1200] : undefined,
-      }, resultsCallback)
-      .call(done);
-  });
-
-  it('should show the press page',function(done) {
-    client
-      .url('http://www.wix.com/demone2/indie-folk#!press/c15e9')
-      .webdrivercss(testName + '.press', {
-        name: '1',
-        exclude:
+        hide:
           [
-            '#comp-ifxnnwb8imageimage'
+            '#submit'
           ],
         screenWidth: selectedCaps == 'chrome' ? [1200] : undefined,
       }, resultsCallback)
       .call(done);
   });
+
+  it('should show the media player',function(done) {
+    client
+      .url('http://www.wix.com/demone2/indie-folk')
+      .pause(10000)
+      .webdrivercss(testName + '.media-player', {
+        name: '1',
+        elem:
+          [
+            '#icxcxffq'
+          ],
+        screenWidth: selectedCaps == 'chrome' ? [1200] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+
 });
